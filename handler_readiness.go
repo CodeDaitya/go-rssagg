6bfa19b9e9d29669db7433c86d5a72d4a8d53d69
Deepak Maurya <itsdpm@gmail.com>
2023-11-15 15:48:23 +0530

    Implement a basic server

---
+++ handler_readiness.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Responding with 5xx error", msg)
+	}
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, errResponse{
+		Error: msg,
+	})
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	respondWithJSON(w, 200, struct{}{})
+}
